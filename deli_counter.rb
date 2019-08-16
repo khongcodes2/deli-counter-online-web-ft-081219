20d8dac4097177b74439f12e4cc1880d721a229b
K Hong <albtrossfacefall@gmail.com>
2019-08-16 09:36:47 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli=[]
+
+def line_string(katz_deli)
+  katz_deli
+end
+
+def line(katz_deli)
+  case katz_deli.length
+    when 0
+      "The line is currently empty"
+    else
+      "The line is currently:" + line_string
+  end
+
+end
+
+def take_a_number(katz_deli,newcomer)
+
+end
