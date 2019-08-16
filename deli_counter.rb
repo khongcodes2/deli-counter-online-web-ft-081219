ef6798e0832d831649b39bbe3e198aedbdf609bc
K Hong <albtrossfacefall@gmail.com>
2019-08-16 09:47:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,6 @@ def line_string(katz_deli)
     new_array << "#{katz_deli.index(name)+1}. #{name}"
   end
   new_array.join(" ")
-  binding.pry
 end
 
 def line(katz_deli)
@@ -16,7 +15,7 @@ def line(katz_deli)
     when 0
       puts "The line is currently empty"
     else
-      "The line is currently:" + line_string
+      "The line is currently:" + line_string(katz_deli)
   end
 
 end
